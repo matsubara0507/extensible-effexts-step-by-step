8d54cd8ae60f4d35c7eebff6da6358c2eafdde4f
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2017-12-07 16:30:16 +0900

    Step0 base interpreter

---
+++ src/Exp.hs
@@ -0,0 +1,15 @@
+module Exp where
+
+import qualified Data.Map as Map
+
+type Name   = String                -- variable names
+data Exp    = Lit Integer           -- expressions
+            | Var Name
+            | Plus Exp Exp
+            | Abs Name Exp
+            | App Exp Exp
+            deriving (Show)
+data Value  = IntVal Integer        -- values
+            | FunVal Env Name Exp
+            deriving (Show)
+type Env    = Map.Map Name Value    -- mapping from names to values
